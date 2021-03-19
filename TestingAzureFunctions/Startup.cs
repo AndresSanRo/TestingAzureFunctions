8bd8782942a65ddee5e4d0f6a2808b6b4131dd4a
Andrés <andressanchezro@hotmail.com>
2021-03-19 15:46:10 +0100

    fea: add startup

---
+++ TestingAzureFunctions/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace TestingAzureFunctions.Fnt
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.TryAdd(ServiceDescriptor.Singleton<ILoggerFactory, LoggerFactory>());
+            builder.Services.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(Logger<>)));
+            builder.Services.AddLogging();                        
+        }
+    }
+}
