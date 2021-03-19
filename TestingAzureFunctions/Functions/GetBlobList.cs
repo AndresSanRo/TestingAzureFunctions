280206bbfe784379083ddc68f08f83faa10d1fd2
Andrés <andressanchezro@hotmail.com>
2021-03-19 17:28:34 +0100

    fea: get containers blob list

--- TestingAzureFunctions/Functions/GetBlobList.cs
+++ TestingAzureFunctions/Functions/GetBlobList.cs
@@ -1,35 +1,35 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Threading.Tasks;
+using TestingAzureFunctions.Services.Abstract;
 
 namespace TestingAzureFunctions.Fnt.Functions
 {
     public class GetBlobList
     {
+        private readonly IBlobStorageService BlobStoregeService;
+        public GetBlobList(IBlobStorageService BlobStoregeService)
+        {
+            this.BlobStoregeService = BlobStoregeService;
+        }
+
         [FunctionName("GetBlobList")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            var client = BlobStoregeService.GetBlobContainerClient();
+            await foreach (BlobItem blobItem in BlobStoregeService.GetBlobsListAsync(client))
+            {
+                log.LogInformation(blobItem.Name);
+            }
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult("Ok");
         }
     }
 }
