1c30e1de6d62f25745780c7c06c6324082843f50
Andrés <andressanchezro@hotmail.com>
2021-03-19 17:43:30 +0100

    fea: return containers blobs list

--- TestingAzureFunctions/Functions/GetBlobList.cs
+++ TestingAzureFunctions/Functions/GetBlobList.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Web.Http;
 using TestingAzureFunctions.Services.Abstract;
 
 namespace TestingAzureFunctions.Fnt.Functions
@@ -17,19 +20,29 @@ namespace TestingAzureFunctions.Fnt.Functions
             this.BlobStoregeService = BlobStoregeService;
         }
 
-        [FunctionName("GetBlobList")]
+        [FunctionName(nameof(GetBlobList))]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            var client = BlobStoregeService.GetBlobContainerClient();
-            await foreach (BlobItem blobItem in BlobStoregeService.GetBlobsListAsync(client))
+            string proccesId = Guid.NewGuid().ToString();
+            try
             {
-                log.LogInformation(blobItem.Name);
-            }
+                log.LogInformation($"{nameof(GetBlobList)} proccess with guid: {proccesId} started at: {DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss tt")}");
+                var client = BlobStoregeService.GetBlobContainerClient();
+                List<string> blobNames = new List<string>();
+                await foreach (BlobItem blobItem in BlobStoregeService.GetBlobsListAsync(client))
+                {
+                    blobNames.Add(blobItem.Name);                    
+                }
 
-            return new OkObjectResult("Ok");
+                return new OkObjectResult(blobNames.ToArray());
+            }
+            catch(Exception ex)
+            {
+                log.LogError($"{nameof(GetBlobList)} proccess with guid: {proccesId} - Error getting the blob list, exception message: {ex.Message}");
+                return new ExceptionResult(ex, false);
+            }            
         }
     }
 }
