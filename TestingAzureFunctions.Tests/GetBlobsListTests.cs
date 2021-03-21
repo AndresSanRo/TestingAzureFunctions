3a425eb1e9d4781e6015c8dfb97aa45a6bff51fe
Andrés <andressanchezro@hotmail.com>
2021-03-21 18:40:19 +0100

    ref: remove test

--- TestingAzureFunctions.Tests/GetBlobsListTests.cs
+++ TestingAzureFunctions.Tests/GetBlobsListTests.cs
@@ -1,13 +1,7 @@
-﻿using Xunit;
-
-namespace TestingAzureFunctions.Tests
+﻿namespace TestingAzureFunctions.Tests
 {
     public class GetBlobsListTests
     {
-        [Fact]
-        public void GetBlobsList_returnArrayOnBlobNames()
-        {
-            var listLogger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
-        }
+       
     }
 }
